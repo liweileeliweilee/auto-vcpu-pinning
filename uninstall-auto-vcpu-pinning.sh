bbf4dc502a6025263b20d84530f7a7be0ed59973
liweileeliweilee <61694171+liweileeliweilee@users.noreply.github.com>
2025-05-30 23:32:26 +0800

    Update uninstall-auto-vcpu-pinning.sh
    sudo rm：
    
    sudo rm -f /usr/local/bin/auto-vcpu-pinning.sh
    sudo rm -f /usr/local/bin/list-core-types.sh

--- uninstall-auto-vcpu-pinning.sh
+++ uninstall-auto-vcpu-pinning.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "🧹 移除 /usr/local/bin/auto-vcpu-pinning.sh..."
-rm -f /usr/local/bin/auto-vcpu-pinning.sh
+sudo rm -f /usr/local/bin/auto-vcpu-pinning.sh
 
 echo "🧹 移除 /usr/local/bin/list-core-types.sh..."
-rm -f /usr/local/bin/list-core-types.sh
+sudo rm -f /usr/local/bin/list-core-types.sh
 
 echo "✅ 已移除 auto-vcpu-pinning 相關腳本。"
